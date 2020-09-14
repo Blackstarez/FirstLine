56062048cc5b6bdad5dafa88497b6a3662886772
junho Do <50814086+djh20@users.noreply.github.com>
2020-09-14 15:33:53 +0900

    전체 글 조회 구현
    전체 글 조회 구현

--- post/views.py
+++ post/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def addPostView(request):
     return render(request, 'post/addPostView.html')
+
+def read_all(request):
+    posts = Post.object.all()
+    return render(request, 'post/read_all.html', {'posts' : posts})
