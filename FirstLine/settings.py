8a0753627fe982f5199f81eb26666cec010d28ef
Blackstarez <qssz1326@naver.com>
2020-09-14 10:27:01 +0900

    설정파일 수정: 앱 추가

--- FirstLine/settings.py
+++ FirstLine/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'member.app.memberConfig',
+    'post.app.postConfig',
+    'report.app.reportConfig',
 ]
 
 MIDDLEWARE = [
--- FirstLine/urls.py
+++ FirstLine/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',),
+    path('member/',print.views.member,name='member'),
 ]
