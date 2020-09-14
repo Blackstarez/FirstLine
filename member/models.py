f4f87a0c2da43d07ed5c054a00781e58d2cc6a67
junho Do <50814086+djh20@users.noreply.github.com>
2020-09-14 13:48:28 +0900

    post model 구현
    post model 구현

--- member/models.py
+++ member/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class MemberInfo(modles.Model):
+class MemberInfo(models.Model):
     object = models.Manager()
     id = models.CharField(max_length = 20, primary_key = True)
     pw = models.CharField(max_length = 16)
@@ -11,7 +11,7 @@ class MemberInfo(modles.Model):
     email = models.CharField(max_length = 30)
     phoneNumber = models.CharField(max_length = 13)
     address = models.CharField(max_length = 100)
-    offerInfoAgree = modles.IntegerField(max_length = 1) # 1: 동의  0: 비동의
+    offerInfoAgree = models.IntegerField(max_length = 1) # 1: 동의  0: 비동의
     offerInfoAgreeDay = models.CharField(max_length = 14)# 년월일시분초  20201116232211
     creationDate = models.CharField(max_length =14)      # 년월일시분초
     classification = models.IntegerField(max_length = 1) # 관리자 회원 구분
