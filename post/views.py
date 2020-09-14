3606550fca035963dfc860cfac7b7b6916c155c3
Blackstarez <qssz1326@naver.com>
2020-09-14 20:55:24 +0900

    sementic_analysis code 작성(진행중)

--- post/views.py
+++ post/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect, resolve_url
 from .models import Post
 from .forms import AddPostForm
+
+# 잘 되게 해주세요
+#import os, sys
+#sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(sementic_analysis))))
+#from .views import sentimenticAnalysis
+
 # Create your views here.
 
 def addPostView(request):
@@ -9,6 +15,7 @@ def addPostView(request):
         if form.is_valid():
             post = Post(title = form.data['title'], content = form.data['content'], user_id = request.session['_auth_user_id'])
             post.save()
+            #sentimenticAnalysis('글번호 넘겨주세요')
             return render(request, 'test/test.html')#이거 바꿔야 함
 
         else :   
--- sentimentic_analysis/views.py
+++ sentimentic_analysis/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+# 잘 되게 해주세요
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(post))))
 from .model import Post
@@ -10,11 +12,12 @@ from .model import Post
 def sentimenticAnalysis(postNum):
     try:
         post = Post.object.get(id=postNum)
+        postSementic=[0,0,0]
         sentences = post['content'].value()
         sentences = sentences.split('\n')
 
         sentiments_P_UP_NU = ['Unpleasure', 'pleasure', 'Neutrality']
-        sentiments_P_NU = ['Pleasure', 'Neutrality']
+        sentiments_P_NU = ['Neutrality', 'Pleasure']
 
         for sentence in sentences:
             sent_data = [[sentence, '1']]
@@ -39,17 +42,23 @@ def sentimenticAnalysis(postNum):
                 prob1_list = max(probabilities1.cpu().detach().tolist())
                 prob1_index = prob1_list.index(max(prob1_list))
                 tag = sentiments_P_UP_NU[prob1_index]
+                postSementic[0] += prob1_list[0]
                 if(prob1_index):
                     #긍정 or 중성이 부정보다 높은경우
                     prob2_list = max(probabilities2.cpu().detach().tolist())
                     prob2_index = prob2_list.index(max(prob2_list))
                     tag = sentiments_P_NU[prob2_index]
-                    newPost = Post(id = )
+                    postSementic[1] += (1 - prob1_list[0])*prob2_list[1] #긍정
+                    postSementic[2] += (1 - prob1_list[0])*prob2_list[0] #중성
                 else:
                     #그렇지 아니한 경우
-
-
-                result.append([sentence, tag])
+                    postSementic[1] += prob1_list[1]
+                    postSementic1[2] += prob1_list[2]
+        newPost = Post(id=post.data['id'],title=post.data['title'],content=post.data['content'],
+        user_id=post.data['user_id'],date_time=post.data['date_time'],prob_p= postSementic[1]/len(sentences),
+        prob_dp= postSementic[0]/len(sentences),prob_n= postSementic[2]/len(sentences),temperature=((28*postSementic[0]/len(sentences))+(36.5*postSementic[2]/len(sentences))+(42*postSementic[1]/len(sentences)))/3)
+        post.delete()
+        newPost.save()
     except :
         pass
     
