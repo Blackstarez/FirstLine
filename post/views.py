afd7545238af672c94d9f573d9ed2c615fac770a
Junho Do <djh20@kumoh.ac.kr>
2020-09-15 20:00:58 +0900

    감성분석 app 삭제

--- post/views.py
+++ post/views.py
@@ -4,9 +4,6 @@ from .forms import AddPostForm
 
 # 잘 되게 해주세요
 import os, sys
-sys.path.append('../sentimentic_analysis/')
-from sentimentic_analysis import views as SentimenticView
-
 import sys
 sys.path.append('../member/')
 sys.path.append('../FirstLine/')
@@ -19,18 +16,10 @@ def addPostView(request):
         form = AddPostForm(request.POST)
         memberInfo = MemberView.get_memberInfo_model(request.session['auth_user_id'])
         if form.is_valid():
-<<<<<<< HEAD
             sentiment_analyszer = SentimentAnalysisModel.SentimentAnalyszer.get_instance()
             prob_n, prob_p, prob_dp, temperature = sentiment_analyszer.get_analysis_result(str(form.data['content']))
             post = Post(title = form.data['title'], content = form.data['content'], user_id = memberInfo, prob_p=prob_p,prob_dp=prob_dp,prob_n=prob_n,temperature=temperature)
             post.save()
-=======
-            post = Post(title = form.data['title'], content = form.data['content'], user_id = memberInfo)
-
-            if SentimenticView.sentimenticAnalysis(post): # 감성분석 성공이든 실패든 등록하게 되는 함수입니다.
-                print('등록성공')
-            else: print('등록실패')
->>>>>>> 7c2286ac2c86279da5ae8777aa1dcbc70a49610d
             return render(request, 'alert_and_redirect.html', {'message' : "성공적으로 등록되었습니다", 'url' : "/"})
         else :   
             form = AddPostForm()     
